5576d844c4c20cd39c9e71523c9b7e88d56ee6e0
brunomaatias <bruno.gabriel0589@gmail.com>
2026-03-01 18:01:18 -0300

    feat: validating infos on ProductRawMaterialResponseDTO.

--- src/main/java/com/brunomatias/autoflex/dtos/ProductRawMaterialResponseDTO.java
+++ src/main/java/com/brunomatias/autoflex/dtos/ProductRawMaterialResponseDTO.java
@@ -1,10 +1,14 @@
 package com.brunomatias.autoflex.dtos;
 
+import jakarta.validation.constraints.Positive;
+
 public record ProductRawMaterialResponseDTO(
         Long productRawMaterialId,
         Long productId,
         String productName,
         Long rawMaterialId,
         String rawMaterialName,
+
+        @Positive
         Integer requiredQuantity
 ) {}
